5cf24d7b2ac2765a64a328900d95aa6ee90115ab
Min-Zheng Shieh <mzshieh@nctu.edu.tw>
2017-06-13 11:40:18 +0000

    sandbox

---
+++ sandbox/install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+install -D pc2exec.py /usr/local/bin/execbox
+install -D pc2java.py /usr/local/bin/javabox
+install -D pc2py2.py /usr/local/bin/py2box
+install -D pc2py3.py /usr/local/bin/py3box
