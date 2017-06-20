edd18563526d0d369a667abef777512536e5e7a1
Min-Zheng Shieh <mzshieh@nctu.edu.tw>
2017-06-20 14:13:23 +0800

    Stress Test Python3

---
+++ sandbox/test_py3.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import os, sys
+from os import fork
+
+BOX = 500
+try:
+    BOX = int(sys.argv[1])
+    sys.argv.pop(1)
+    if BOX > 1000:
+        BOX = 1000
+except:
+    pass
+
+for i in range(BOX):
+    os.mkdir('r{:03d}'.format(i))
+    pid = fork()
+    if pid == 0:
+        os.chdir('r{:03d}'.format(i))
+        os.system('cp ../{} .'.format(sys.argv[1]))
+        os.system('ioibox -b={} python3 {} < {} > SANDBOX_OUTPUT'
+                  .format(i,sys.argv[1],sys.argv[2]))
+        break
